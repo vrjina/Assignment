61cfd9c9f381c74d6f768a48e54430287f2a3d3a
vrjina <jinarival@gmail.com>
2023-04-04 17:29:53 +0530

    Demo.java modified

--- Demo.java
+++ Demo.java
@@ -4,6 +4,7 @@ public static void main(String[]args){
 
 System.out.println("Renu loves vimal");
 System.out.println("Vimal will become the greatest fighter");
+System.out.println("renu weds vimal");
 
 }
 }
